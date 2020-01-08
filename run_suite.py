ac79604d4e2c488b05dcbac7a4a3af11ab1dd63d
我你 <123!@123.com>
2020-01-08 12:20:16 +0800

    time

--- run_suite.py
+++ run_suite.py
@@ -13,8 +13,8 @@ suite.addTest(unittest.makeSuite(TestLogin)) # 登录测试
 suite.addTest(unittest.makeSuite(TestGetTkoen)) # 令牌
 suite.addTest(unittest.makeSuite(TestEmp)) # 员工测试
 
-file_dir = BASE_DIR + '/report/{}.html'.format(time.strftime('%Y%m%d_%H%M%S'))
-
+# file_dir = BASE_DIR + '/report/{}.html'.format(time.strftime('%Y%m%d_%H%M%S'))
+file_dir = BASE_DIR + '/report/ihrm.html'
 with open(file_dir,'wb') as f:
     runner = HTMLTestReportCN(f,1,'ITMH测试报告','Win8','QAhyc')
     runner.run(suite)
