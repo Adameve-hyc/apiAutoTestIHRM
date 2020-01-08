10b9da3db87a67cddc92b1d7b74a5d8a9aed0fc4
我你 <123!@123.com>
2020-01-08 08:29:40 +0800

    add search emp list

--- read_data/read_emp.py
+++ read_data/read_emp.py
@@ -4,6 +4,7 @@ from app import BASE_DIR
 
 
 def read_add_emp():
+    '''读取添加员工数据'''
     file_dir = BASE_DIR + '/data/emp_data.json'
     with open(file_dir, encoding='utf-8') as f:
         read_add = list()
@@ -20,6 +21,7 @@ def read_add_emp():
 
 
 def read_search_emp():
+    '''读取查询员工数据'''
     file_dir = BASE_DIR + '/data/emp_data.json'
     with open(file_dir, encoding='utf-8') as f:
         read_search = list()
@@ -34,6 +36,7 @@ def read_search_emp():
 
 
 def read_update_emp():
+    '''读取修改员工数据'''
     file_dir = BASE_DIR + '/data/emp_data.json'
     with open(file_dir, encoding='utf-8') as f:
         read_update = list()
@@ -49,6 +52,7 @@ def read_update_emp():
 
 
 def read_delete_emp():
+    '''读取删除员工数据'''
     file_dir = BASE_DIR + '/data/emp_data.json'
     with open(file_dir, encoding='utf-8') as f:
         read_delete = list()
@@ -60,3 +64,40 @@ def read_delete_emp():
                             data.get('desc')
                             ))
         return read_delete
+
+
+def read_emp_list():
+    '''读取查询员工列表size返回条数数据'''
+    file_dir = BASE_DIR + '/data/emp_data.json'
+    with open(file_dir, encoding='utf-8') as f:
+        read_emp_list = list()
+        data = json.load(f).get('emp_list')
+        for i in data:
+            read_emp_list.append((i.get('type'),
+                                  i.get('size'),
+                                  i.get('status_code'),
+                                  i.get('success'),
+                                  i.get('code'),
+                                  i.get('message'),
+                                  i.get('desc')
+                                  ))
+        return read_emp_list
+
+
+def read_mobile_find_username():
+    '''根据手机号查用户名'''
+    file_dir = BASE_DIR + '/data/emp_data.json'
+    with open(file_dir, encoding='utf-8') as f:
+        read_date = list()
+        data = json.load(f).get('mobile_find_username')
+        read_date.append((data.get('type'),
+                          data.get('size'),
+                          data.get('status_code'),
+                          data.get('success'),
+                          data.get('code'),
+                          data.get('message'),
+                          data.get('mobile'),
+                          data.get('expect'),
+                          data.get('desc')
+                          ))
+        return read_date
